4362b9a29f2fb4b005568c3fda8d34dae15d45e7
Ling Ren <renling@ubuntu.(none)>
2014-03-23 16:46:25 -0400

    Test for highest common path

---
+++ CommonPathTest.py
@@ -0,0 +1,15 @@
+
+f = open("CommonPathResult.txt")
+results = f.read().splitlines()
+f.close()
+results = [res.split(' ') for res in results]
+
+for res in results:
+	leaf0, leaf1, maxCommonLevel = int(res[0]), int(res[1]), int(res[2])  	
+			# format: [leaf0, leaf1, maxCommonLevel] Leaf is labeled with bucket ID. Root bucket is Bucket 0, Level 0.
+			# My root bucket is Bucket 0; If yours is Bucket 1, increment leaf0 and leaf1 
+
+	yourMaxCommonLevel = 0; # Your implementation
+	
+	if yourMaxCommonLevel != maxCommonLevel:
+		print("results differ on \t {0} ^ {1}: {2} vs. {3}".format(leaf0, leaf1, yourMaxCommonLevel, maxCommonLevel))
