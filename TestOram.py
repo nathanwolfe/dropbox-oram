ea2cac1ff95c49e593a142dfce3521beafed048a
yxy <yxy@yxy-VirtualBox.(none)>
2014-03-30 17:36:29 -0400

    test file

--- TestOram.py
+++ TestOram.py
@@ -1,10 +1,42 @@
 import Oram
 
-def TestBasic() :
-    oram = Oram.Oram(7, 4)
-    for key in range(0, 7) :
-        oram.write(key, str(key))
-    for key in range(0, 7) :
-        assert (oram.read(key) == str(key))
 
+def TestBasic() :
+    oramsize = 1 << 4 - 1
+    oram = Oram.Oram(oramsize, 4)
+    for key in range(0, oramsize) :
+        oram.write(key, str(key))
+    for key in range(0, oramsize) :
+        try :
+            getvalue = oram.read(key)
+            assert (getvalue == str(key))
+        except :
+            print( "[TestBasic] key=%d. expecting %s but got %s" % (key, str(key), getvalue) )
+            print( "TestBasic failed." )
+    print( "TestBasic Passed." )
+"""
+def TestRepeatRW() :
+    oramsize = 1 << 4 - 1
+    oram = Oram.Oram(oramsize, 4)
+    db = {}
+    for key in range(0, oramsize) :
+        oram.write(key, str(key))
+    for key in range(0, oramsize) :
+        try :
+            getvalue = oram.read(key)
+            assert (getvalue == str(key))
+            oram.write(key, 'v')
+        except :
+            print( "[TestRepeatRW] key=%d. expecting %s but got %s" % (key, str(key), getvalue) )
+            return
+    
+
+def TestLargeTree() :
+    oramsize=1024*1024 - 1
+    oram = Oram.Oram(oramsize, 4)
+    for key in range(0, 1024 * 128) :
+        oram.write(key, "v"+str(key))
+    # TODO 
+"""
 TestBasic()
+# TestRepeatRW()
